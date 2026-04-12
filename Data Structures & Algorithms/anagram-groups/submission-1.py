b12ea3a55135711378f49d2ddca85fdcc4e66336
Abhishek Kapil <abhishek5kapil@gmail.com>
2026-04-12 10:01:45 -0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        run strs loop with each str
+        run 26 char array, put char in the new array if its in the str
+        check if the new array is a key in store and add 2, append these
+        store array as key and value as 1
+        """
+        store = dict()
+        characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+        
+        for str in strs:
+
+            arrChar = [0] * 26
+            arrStr = list()
+
+            for c in str:
+                arrChar[ord(c) - ord('a')] += 1
+
+            my_arrChar = tuple(arrChar)
+            print(my_arrChar)
+            if my_arrChar in store:
+                store[my_arrChar].append(str)
+            else:
+                store[my_arrChar] = [str]
+        
+        result = list()
+        for value in store.values():
+            result.append(value)
+
+        return result
+
